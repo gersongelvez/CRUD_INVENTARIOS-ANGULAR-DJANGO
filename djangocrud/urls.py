9a0f272909cbae966dba762574c8205971e2934b
gersongelvez <37311458+gersongelvez@users.noreply.github.com>
2019-08-18 01:03:46 -0500

    Se actualiza la configuracion de Swagger
    Se actualiza la configuracion de Swagger

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from djangocrud.api import views
+from rest_framework_swagger.views import get_swagger_view
+from rest_framework.schemas.coreapi import AutoSchema
 
 router = routers.DefaultRouter()
 router.register(r'movies', views.MoviesViewSet)
@@ -12,8 +14,11 @@ router.register('area', views.AreaViewSet)
 router.register('persona', views.PersonaViewSet)
 router.register('bien', views.BienViewSet)
 
+schema_view = get_swagger_view(title="Inventarios Swagger")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', schema_view)
 ]
